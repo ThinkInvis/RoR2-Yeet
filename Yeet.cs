22b739fb8a3d4a096418925a331e5e8a8713d974
ThinkInvisible <ThinkInvis@users.noreply.github.com>
2020-07-12 16:28:29 -0500

    Implement scaling throw speed based on click duration

--- Yeet.cs
+++ Yeet.cs
@@ -21,7 +21,9 @@ namespace ThinkInvisible.Yeet {
         public const string ModGuid = "com.ThinkInvisible.Yeet";
 
         //static float regrabCooldown = 5f;
-        static float throwForce = 30f;
+        static float lowThrowForce = 20f;
+        static float highThrowForce = 100f;
+        static float highThrowTime = 2f;
         static bool preventLunar = true;
 
         internal static ManualLogSource _logger;
@@ -34,15 +36,24 @@ namespace ThinkInvisible.Yeet {
                 "Time (in seconds) to prevent picking an item back up after dropping it. Does not apply to other players, only the dropper. Set to <= 0 to disable.",
                 new AcceptableValueRange<float>(0f,float.MaxValue)));*/
 
-            var cfgThrowForce = cfgFile.Bind(new ConfigDefinition("Yeet", "ThrowForce"), 50f, new ConfigDescription(
-                "Initial speed, in player view direction, to add to droplets for dropped items.",
+            var cfgLowThrowForce = cfgFile.Bind(new ConfigDefinition("Yeet", "LowThrowForce"), 30f, new ConfigDescription(
+                "Minimum speed, in player view direction, to add to droplets for dropped items.",
+                new AcceptableValueRange<float>(0f,float.MaxValue)));
+            var cfgHighThrowForce = cfgFile.Bind(new ConfigDefinition("Yeet", "HighThrowForce"), 150f, new ConfigDescription(
+                "Maximum speed, in player view direction, to add to droplets for dropped items.",
+                new AcceptableValueRange<float>(0f,float.MaxValue)));
+            var cfgHighThrowTime = cfgFile.Bind(new ConfigDefinition("Yeet", "HighThrowTime"), 2f, new ConfigDescription(
+                "Click hold time (sec) required to reach HighThrowForce.",
                 new AcceptableValueRange<float>(0f,float.MaxValue)));
 
             var cfgPreventLunar = cfgFile.Bind(new ConfigDefinition("Yeet", "PreventLunar"), true, new ConfigDescription(
                 "If true, lunar items cannot be dropped (to preserve the consequences of picking one up)."));
             
             //regrabCooldown = cfgRegrabCooldown.Value;
-            throwForce = cfgThrowForce.Value;
+            lowThrowForce = cfgLowThrowForce.Value;
+            highThrowForce = cfgHighThrowForce.Value;
+            highThrowTime = cfgHighThrowTime.Value;
+
             preventLunar = cfgPreventLunar.Value;
 
             On.RoR2.UI.ItemIcon.Awake += ItemIcon_Awake;
@@ -61,7 +72,7 @@ namespace ThinkInvisible.Yeet {
                 return;
             }
             ItemIndex ind;
-            string itemSearch = args.TryGetArgString(1);
+            string itemSearch = args.TryGetArgString(0);
             if(itemSearch == null) {
                 _logger.LogError("ConCmdYeet: could not read first argument (item ID)!");
                 return;
@@ -94,6 +105,13 @@ namespace ThinkInvisible.Yeet {
             if(idef.hidden || (idef.tier == ItemTier.Lunar && preventLunar)) return;
             args.senderBody.inventory.RemoveItem(ind);
             
+            float throwForce = lowThrowForce;
+            if(args.Count > 1) {
+                var tfarg = args.TryGetArgFloat(1);
+                if(tfarg.HasValue) 
+                    throwForce = Mathf.Lerp(lowThrowForce, highThrowForce, Mathf.Clamp01(tfarg.Value));
+            }
+
             PickupDropletController.CreatePickupDroplet(
                 PickupCatalog.FindPickupIndex(ind),
                 args.senderBody.inputBank.aimOrigin,
@@ -101,15 +119,20 @@ namespace ThinkInvisible.Yeet {
         }
     }
     
-	public class YeetButton : MonoBehaviour, IPointerClickHandler {
-		void IPointerClickHandler.OnPointerClick(PointerEventData eventData) {
+	public class YeetButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+        float holdTime = 0f;
+		void IPointerDownHandler.OnPointerDown(PointerEventData eventData) {
+            holdTime = Time.unscaledTime;
+		}
+        void IPointerUpHandler.OnPointerUp(PointerEventData eventData) {
+            float totalTime = Time.unscaledTime - holdTime;
             var icon = this.GetComponentInParent<RoR2.UI.ItemIcon>();
             var ind = icon.GetFieldValue<ItemIndex>("itemIndex");
 			if(NetworkUser.readOnlyLocalPlayersList.Count > 0)
-                RoR2.Console.instance.SubmitCmd(NetworkUser.readOnlyLocalPlayersList[0], "yeet " + ((int)ind).ToString());
+                //RoR2.Console.instance.RunClientCmd(NetworkUser.readOnlyLocalPlayersList[0], "yeet", new string[]{((int)ind).ToString(), totalTime.ToString("N3")});
+                RoR2.Console.instance.SubmitCmd(NetworkUser.readOnlyLocalPlayersList[0], "yeet " + ((int)ind).ToString() + " " + totalTime.ToString("N4"));
             else
                 YeetPlugin._logger.LogError("Received inventory click event with no active local players!");
-            
-		}
+        }
     }
 }
